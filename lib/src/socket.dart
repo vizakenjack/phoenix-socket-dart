297cd2e4b34cb456d65a2ef5257d3e37160a9072
Farolan Faisal <farolanfaisal@gmail.com>
2021-10-18 06:42:05 +0800

    fix slow reconnection

--- lib/src/socket.dart
+++ lib/src/socket.dart
@@ -422,9 +422,17 @@ class PhoenixSocket {
     }
 
     try {
-      await sendMessage(_heartbeatMessage());
-      _logger.fine('[phoenix_socket] Heartbeat completed');
-      return true;
+      return await Future.any<bool>([
+        Future(() async {
+          await sendMessage(_heartbeatMessage());
+          _logger.fine('[phoenix_socket] Heartbeat completed');
+          return true;
+        }),
+        Future.delayed(_options.timeout, () {
+          _logger.severe('[phoenix_socket] Heartbeat timed out');
+          return false;
+        })
+      ]);
     } on PhoenixException catch (err, stacktrace) {
       _logger.severe(
         '[phoenix_socket] Heartbeat message failed with error',
